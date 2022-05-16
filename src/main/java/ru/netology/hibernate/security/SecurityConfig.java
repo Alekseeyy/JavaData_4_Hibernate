9417222a561baf6c4614ac9efa9fe3682940aba4
Aleksey Falev <aleksej.falev@mail.ru>
2022-05-16 21:25:47 +0300

    Spring Security

---
+++ src/main/java/ru/netology/hibernate/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package ru.netology.hibernate.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Aleksey").password("{noop}password").authorities("read", "write")
+                .and()
+                .withUser("Dima").password("{noop}password2").authorities("read");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests().antMatchers("/hi").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}

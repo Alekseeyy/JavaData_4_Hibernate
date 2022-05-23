896a3397bf15757fd20bf5c4c466a7b719565e23
Aleksey Falev <aleksej.falev@mail.ru>
2022-05-23 18:22:42 +0300

    Method security

--- src/main/java/ru/netology/hibernate/security/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/security/SecurityConfig.java
@@ -1,19 +1,37 @@
 package ru.netology.hibernate.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Aleksey").password("{noop}password").authorities("read", "write")
+                .withUser("Aleksey")
+                .password("{noop}password")
+                .roles("READ", "WRITE", "DELETE")
                 .and()
-                .withUser("Dima").password("{noop}password2").authorities("read");
+                .withUser("Dima")
+                .password(encoder().encode("password2"))
+                .roles("READ")
+                .and()
+                .withUser("Olya")
+                .password(encoder().encode("password3"))
+                .roles("READ", "WRITE");
     }
 
     @Override
